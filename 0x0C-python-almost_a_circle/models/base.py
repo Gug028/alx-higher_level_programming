069d093a3fe04fcbe6bb5e55d59d8a4aa283fa34
Gug028 <gramthembu@gmail.com>
2024-02-21 15:18:52 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module models.base
+This module contains the Base class
+"""
+import json
+
+
+class Base:
+    """
+    base class for managing id attribute in all future classes.
+
+    Attributes:
+    __nb_objects (int): Private class attribute
+    id (int): Public instance attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for Base class.
+
+        Args:
+        id (int, optional): The ID value to be assigned to the instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert list dictionary to json"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
