58f48c6c2cddb46763063872baa5866db2ceaa3d
Gug028 <gramthembu@gmail.com>
2024-02-23 10:01:48 -0800

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Creaates DataBase and User
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd";
-GRANT SELECT ON hbtn_0d_2.* to user_0d_2@localhost;
+-- create user
+CREATE USER IF NOT EXISTS user_0d_1@locasthost IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
