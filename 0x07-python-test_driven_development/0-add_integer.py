b766c1370c2aa6983ef061f052d47f2cf9497b3f
Gug028 <gramthembu@gmail.com>
2024-04-05 09:10:27 -0700

    Add integer to fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 
-"""0-add_integer
-The function "add_integer" returns the sum of two integers.
+
+"""
+It takes 2 integers a and b
+It returns the sum of the 2 integers with a type of int]
+If convert a and b to int if they are float
+
 """
 
 
 def add_integer(a, b=98):
-    """adds two integers function body"""
 
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    '''
+    Add two numbers a and b
+    test if a is an int or a float
+    test if b is an int ora float
+    convert a and b to int if they are float
+    '''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a  must be an integer")
+
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
-    return int(a) + int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
