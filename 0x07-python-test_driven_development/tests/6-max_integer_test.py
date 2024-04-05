f314d6fe4e36e7ac2440c5d545c917a05ed98f9b
Gug028 <gramthembu@gmail.com>
2024-04-05 13:24:31 -0700

    Print square

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
-"""
-Import max_integer
-"""
-
-
 class TestMaxInteger(unittest.TestCase):
-    """
-    Test max integer
-
-    """
-    def test_regular(self):
-        """ regular test"""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_reversed(self):
-        """reversed test"""
-        self.asserEqual(max_integer([4, 3, 2, 1]), 4)
-
-    def test_unsorted_list(self):
-        """unsorted list"""
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_empty_list(self):
-        """empty list"""
+        """
+        Test for an empty list. Should return None.
+        """
         self.assertIsNone(max_integer([]))
 
-    def test_negative_numbers(self):
-        """negative test"""
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    def test_single_element_list(self):
+        """
+        Test for a list with a single element. Should return the element itself.
+        """
+        self.assertEqual(max_integer([5]), 5)
 
-    def test_mixed_positive_negative(self):
-        """mixed numbers test"""
-        self.assertEqual(max_integer([01, 2, -3, 4]), 4)
+    def test_positive_numbers(self):
+        """
+        Test for a list of positive numbers. Should return the maximum.
+        """
+        self.assertEqual(max_integer([3, 5, 2, 8, 1]), 8)
+
+    def test_negative_numbers(self):
+        """
+        Test for a list of negative numbers. Should return the maximum (the least negative).
+        """
+        self.assertEqual(max_integer([-3, -5, -2, -8, -1]), -1)
+
+    def test_mixed_numbers(self):
+        """
+        Test for a list of mixed positive and negative numbers. Should return the maximum positive.
+        """
+        self.assertEqual(max_integer([3, -5, 2, -8, 1]), 3)
+
+    def test_duplicate_maximum(self):
+        """
+        Test for a list with duplicate maximum values. Should return the maximum.
+        """
+        self.assertEqual(max_integer([3, 5, 2, 8, 8]), 8)
+
+    def test_float_numbers(self):
+        """
+        Test for a list of floating-point numbers. Should return the maximum.
+        """
+        self.assertAlmostEqual(max_integer([1.5, 2.5, 3.5]), 3.5)
+
+if __name__ == '__main__':
+    unittest.main()
