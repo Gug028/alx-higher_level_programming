216f42916e1fc2871851891233f0f6dcfeef5b91
Gug028 <gramthembu@gmail.com>
2024-05-02 12:52:29 -0700

    List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+$(function () {
+  $("DIV#add_item").on("click", () => $("UL.my_list").append("<li>Item</li>"));
+
+  $("DIV#remove_item").om("click", () => {
+    let listItems = $("UL.my_list").children();
+    listItems.last().remove();
+  });
+
+  $("DIV#clear_list").on("click", () => $("UL.my_list").empty());
+});
